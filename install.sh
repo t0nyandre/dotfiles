fc0c682c7a61061352d2e708d60eb67c0a15e352
t0nyandre <post@tonyand.re>
2018-06-13 21:01:00 +0200

    fixed some errors in install script

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 setup_fish() {
 	echo "Setting up fish" \
-		&& rm -rf ~/.config/fish/config.fish \
+		&& rm -rf ~/.config/fish/config.fish ~/.config/fish/functions \
 		&& ln -s $(pwd)/config.fish ~/.config/fish/config.fish \
 		&& ln -s $(pwd)/functions ~/.config/fish/functions \
-		&& curl -L https://get.oh-my.fish | fish \
-		&& omf install clearance
+		&& curl -L https://get.oh-my.fish | fish & \
+    omf install clearance &
 }
 
 install_fonts() {
 	echo "Setting up fonts" \
+    && mkdir ~/.local/share/fonts \
 		&& rm -rf ~/.local/share/fonts/FuraMono-* \
 		&& cp $(pwd)/fonts/* ~/.local/share/fonts/
 }
@@ -31,6 +32,7 @@ setup_neovim() {
 	echo "Setting up NeoVim" \
 		&& rm -rf ~/.config/nvim \
 		&& curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_installer.sh \
+		&& chmod +x dein_installer.sh \
 		&& ./dein_installer.sh ~/.config/nvim/bundles \
 		&& ln -s $(pwd)/autoload ~/.config/nvim/autoload \
 		&& ln -s $(pwd)/init.vim ~/.config/nvim/init.vim \
@@ -39,9 +41,9 @@ setup_neovim() {
 
 setup_lsp() {
 	echo "Setting up LSP servers" \	
-		&& echo "Setting up PHP-Language-Server" \
+		echo "Setting up PHP-Language-Server" \
 		&& composer require jetbrains/phpstorm-stubs:dev-master \
-		&& composer require felixfbecker/language_server \
+		&& composer require felixfbecker/language-server \
 		&& composer run-script --working-dir=vendor/felixfbecker/language-server parse-stubs
 }
 
@@ -49,7 +51,7 @@ echo -n "This will delete all your previous NeoVim, termite and fish configurati
 read answer
 if echo "$answer" | grep -iq "^y" ;then
 	echo "Installing dependencies ..." \
-		&& pacman -Sy neovim xclip python2-neovim python-neovim ripgrep fzf composer \
+		&& sudo pacman -Sy neovim xclip python2-neovim python-neovim ripgrep fzf composer \
 		&& setup_fish \
 		&& install_fonts \
 		&& setup_termite \
